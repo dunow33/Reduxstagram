d2b122b12dcf4fe3b62f7ab91cffe6abfa745a8f
Brian Donnoe <dunow33@hotmail.com>
2019-06-13 23:44:27 -0400

    adding redux stuff

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -1,12 +1,23 @@
-function increment(index) {
+export function increment(index) {
 	return {
 		type: 'INCREMENT_LIKES',
 		index
 	}
 }
 
-function addComment(postId, author, comment) {
+export function addComment(postId, author, comment) {
 	return {
-		type: 'ADD_COMMENT'
+		type: 'ADD_COMMENT',
+		postId,
+		author,
+		comment
+	}
+}
+
+export function removeComment(postId, i) {
+	return {
+		type: 'REMOVE+COMMENT',
+		i,
+		postId
 	}
 }
